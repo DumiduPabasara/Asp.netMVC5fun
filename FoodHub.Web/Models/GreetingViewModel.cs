d2b55fd7e34e2e940e6b64284baba497fcd83c74
Dumidu Pabasara <eagleshadowtrigono@gmail.com>
2021-04-14 18:49:21 +0530

    understanding mvc controllers

--- FoodHub.Web/Models/GreetingViewModel.cs
+++ FoodHub.Web/Models/GreetingViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using FoodHub.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace FoodHub.Web.Models
 {
-    public class GreetingViewModel
-    {
+    public class GreetingViewModel 
+    { 
+
+        public IEnumerable<Restaurant> Restaurants { get; set; }
         public string Message { get; set; }
         public string Name { get; set; }
     }
